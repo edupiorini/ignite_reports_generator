8bd1eee183d14ffb394c886910396772cb4b04fc
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-12 07:09:07 -0300

    refactoring

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,18 +1,12 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.reduce(report_acc(), fn line, report ->
-      [id, _food_name, price] = parse_line(line)
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn [id, _food_name, price], report ->
       Map.put(report, id, report[id] + price)
     end)
-  end
-
-  defp parse_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(2, &String.to_integer/1)
   end
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
