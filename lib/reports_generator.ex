1a010b3086d1fb610d3f879d9dae65097b96c57b
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-12 05:51:02 -0300

    handle file now is created

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -4,4 +4,7 @@ defmodule ReportsGenerator do
     |> File.read()
     |> handle_file()
   end
+
+  defp handle_file({:ok, file_content}), do: file_content
+  defp handle_file({:error, _reason}), do: "Error while opening file!"
 end
