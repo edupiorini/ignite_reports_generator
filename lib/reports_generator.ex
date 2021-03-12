164a251d3352fd8311664fae5d718f957e2578f1
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-12 05:41:50 -0300

    case for error hadling

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,5 +1,8 @@
 defmodule ReportsGenerator do
   def build(filename) do
-    file = File.read("reports/#{filename}")
+    case file = File.read("reports/#{filename}") do
+      {:ok, result} -> result
+      {:error, reason} -> reason
+    end
   end
 end
