acfa0decafb628f1c92d6642124a866a1fbd96ab
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-12 07:12:03 -0300

    refactoring and creating sum_values

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -4,10 +4,10 @@ defmodule ReportsGenerator do
   def build(filename) do
     filename
     |> Parser.parse_file()
-    |> Enum.reduce(report_acc(), fn [id, _food_name, price], report ->
-      Map.put(report, id, report[id] + price)
-    end)
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
+
+  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
